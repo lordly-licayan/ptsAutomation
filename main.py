5392f6dcac86e1e810ab7bbfacc066ad317be670
l.licayan <l.licayan@ph.fujitsu.com>
2019-07-01 10:59:02 +0800

    corrected files.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Welcome to PTS Automation.")
+import sys, getopt
+
+def main(argv):
+   inputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"i:",["ifile="])
+
+   except getopt.GetoptError:
+      print ('main.py -i <inputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print ('main.py -i <PTS input file.xlsx>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+
+   print ('PTS file is ', inputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
