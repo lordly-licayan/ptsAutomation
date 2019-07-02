65e2c01ebcc905a11bbe812d7a3a7fb124f7a0a3
l.licayan <l.licayan@ph.fujitsu.com>
2019-07-02 13:48:03 +0800

    updated main

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import sys, getopt
+from config import configSetup as config
 
 def main(argv):
-   inputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"i:",["ifile="])
+    inputfile = ''
+    configFile= "ptsAutomation\config\config.xml"
+   
+    try:
+        opts, args = getopt.getopt(argv,"i:",["ifile="])
 
-   except getopt.GetoptError:
-      print ('main.py -i <inputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print ('main.py -i <PTS input file.xlsx>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
+    except getopt.GetoptError:
+        print ('main.py -i <inputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('main.py -i <PTS input file.xlsx>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
 
-   print ('PTS file is ', inputfile)
+    print ('PTS file is ', inputfile)
+    configMap= config.parseXML(configFile)
+    print(configMap)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
